e74712fbefe7ea67974587a816f587ff66df1106
florianeich <florianeich@small.flo.debian.com>
2012-12-03 13:54:20 +0100

    aktuelle version flo_working

--- performConnection.c
+++ performConnection.c
@@ -16,78 +16,67 @@ Fehlerbehandlung mit __FILE__ und __LINE__ (und __METHOD__)
 strace verwenden
 */
 
+// msg, argv, subargv werden von so gut wie allen Funktionen verwendet
+char msg[BUFLEN];
+char *argv[BUFLEN];
+char *subargv[BUFLEN];
+
 int 
 performConnection(int socketFD, char *gameID){
-  // benoetigte variablen
-  // int i;
-  char *argv[BUFLEN];
-  // char *subargv[BUFLEN];
-  char msg[BUFLEN] = {0};
+  // benoetigte Variablen
+  int i, argc;
+  char *farbe;
   int readyflag = 0;
   
   // Version und Game-ID mit dem Server abklaeren
   readyflag = openingHandler(socketFD, gameID);
   
-  readyflag = receive(socketFD, msg, argv);
-  printf("%d: %s\n", readyflag, argv[0]);
-  
-  readyflag = receive(socketFD, msg, argv);
-  printf("%d: %s\n", readyflag, argv[0]);
-  
   // Spielerdaten mit dem Server austauschen und Prolog beenden
-  // while(readyflag > 0 && (readyflag = receive(socketFD, argv)) > 0){
-  //     int argc;
-  //     int playerflag = 0;
-  //     printf("%s\n", argv[0]);
-  //     argc = stringSplit(argv[0], subargv, " ");
-  //     printf("%s\n", subargv[0]);
-  //     printf("%s\n", subargv[1]);
-  //       char *farbe;
-  //       for(i = 0; i < readyflag; i++){
-  //         argc = stringSplit(argv[i], subargv, " ");
-  //         if(playerflag > 1)
-  //           playerflag = printPlayers(playerflag, subargv);
-  //         else{
-  //           for(int j = 0; j < argc; j++) 
-  //             printf("%s\n", subargv[j]);
-  //           switch(checkMsg(subargv[1])){
-  //             case -1:
-  //               fprintf(stdout, "Uebler Fehler.\n\n");
-  //               return EXIT_FAILURE;
-  //             break;
-  //             case 0:
-  //               fprintf(stdout, "\nServer sendet %s. Damit ist der Prolog beendet.\n\n", subargv[1]);
-  //               return EXIT_SUCCESS;  // TEMPORARY SOLUTION
-  //             break;
-  //             case 1:
-  //               if(strcmp(subargv[2], GAMEKINDNAME) != 0){
-  //                 fprintf(stdout, "Der Server will %s mit uns spielen. Das wollen wir nicht.\n", subargv[2]);
-  //                 return EXIT_FAILURE;
-  //               }
-  //               fprintf(stdout, "\nWir spielen %s. ", subargv[2]);
-  //               if(receive(socketFD, argv)){
-  //                 stringSplit(argv[0], subargv, " ");
-  //                 fprintf(stdout, "Der Name des Spiels lautet %s.\n\n", subargv[1]);
-  //                 sendToServer(socketFD, "PLAYER\n");
-  //               } else
-  //                 fprintf(stdout, "Fehler beim Empfangen des Spielnamens.\n");
-  //             break;
-  //             case 2:
-  //               farbe = (strcmp(subargv[3], "white")) ? "Schwarz" : "Weiss";
-  //               fprintf(stdout, "\nSie sind Spieler Nr. %i und spielen die Farbe %s.\n\n",
-  //                 atoi(subargv[2]), farbe);
-  //             break;
-  //             case 3:
-  //               playerflag = atoi(subargv[2]);
-  //               fprintf(stdout, "Insgesamt nehmen %i Spieler am Spiel teil:\n", playerflag);
-  //             break;
-  //             default:
-  //               fprintf(stdout, "Fehler, weißte Bescheid unso\n");
-  //             break;
-  //           }
-  //         }
-  //       }
-  // }
+  while(readyflag > 0 && (readyflag = receive(socketFD)) > 0){
+    int playerflag = 0;
+    for(i = 0; i < readyflag; i++){
+      argc = stringSplit(argv[i], subargv, " ");
+      if(playerflag > 1)
+        playerflag = printPlayers(playerflag, subargv);
+      else{
+        switch(checkMsg(subargv[1])){
+          case -1:
+            fprintf(stdout, "Uebler Fehler.\n\n");
+            return EXIT_FAILURE;
+          break;
+          case 0:
+            fprintf(stdout, "\nServer sendet %s. Damit ist der Prolog beendet.\n\n", subargv[1]);
+            return EXIT_SUCCESS;  // TEMPORARY SOLUTION
+          break;
+          case 1:
+            if(strcmp(subargv[2], GAMEKINDNAME) != 0){
+              fprintf(stdout, "Der Server will %s mit uns spielen. Das wollen wir nicht.\n", subargv[2]);
+              return EXIT_FAILURE;
+            }
+            fprintf(stdout, "\nWir spielen %s. ", subargv[2]);
+            if(receive(socketFD) > 0){
+              stringSplit(argv[0], subargv, " ");
+              fprintf(stdout, "Der Name des Spiels lautet %s.\n\n", subargv[1]);
+              sendToServer(socketFD, "PLAYER\n");
+            } else
+              fprintf(stdout, "Fehler beim Empfangen des Spielnamens.\n");
+          break;
+          case 2:
+            farbe = (strcmp(subargv[3], "white")) ? "Schwarz" : "Weiss";
+            fprintf(stdout, "\nSie sind Spieler Nr. %i und spielen die Farbe %s.\n\n",
+              atoi(subargv[2]), farbe);
+          break;
+          case 3:
+            playerflag = atoi(subargv[2]);
+            fprintf(stdout, "Insgesamt nehmen %i Spieler am Spiel teil:\n", playerflag);
+          break;
+          default:
+            fprintf(stdout, "Fehler, weißte Bescheid unso\n");
+          break;
+        }
+      }
+    }
+  }
   
   // Behandlung von Fehlermeldung vom Server
   if(readyflag == 0){
@@ -106,19 +95,17 @@ performConnection(int socketFD, char *gameID){
 
 int
 openingHandler(int socketFD, char *gameID){
-  char msg[BUFLEN] = {0};
-  char *argv[BUFLEN];
   int flag;
   
   // Version
-  if((flag = receive(socketFD, msg, argv)) > 0){
+  if((flag = receive(socketFD)) > 0){
     sprintf(msg, "%s %s\n", "VERSION", VERSION);
     sendToServer(socketFD, msg);
   } else    
     return flag;
   
   //GameID
-  if((flag = receive(socketFD, msg, argv)) > 0){
+  if((flag = receive(socketFD)) > 0){
     sprintf(msg, "%s %s\n", "ID", gameID);
     sendToServer(socketFD, msg);
   }
@@ -127,22 +114,18 @@ openingHandler(int socketFD, char *gameID){
 }
 
 int
-receive(int socketFD, char *msg, char *argv[]){
+receive(int socketFD){
   int size;
   memset(msg, 0, BUFLEN);
   if((size = recv(socketFD, msg, BUFLEN-1, 0)) == -1){
-    // fprintf(stdout, "error in %s at %d", __FILE__, __LINE__);
     perror("perror");
     return -1;
   }
   
-  // for(int i = 0; msg[i] != '\0'; i++)  // HIER IST DER LOOP, DER MACHT, DASS ES FUNKTIONIERT
-  //     printf("%c", msg[i]);            // ich kapier nicht, warum.
-  
   size = stringSplit(msg, argv, "\n");
   
-  for(int i = 0; i < size; i++)
-    fprintf(stdout, "Empfangene Nachricht: %s\n", argv[i]);
+/*  for(int i = 0; i < size; i++)*/
+/*    fprintf(stdout, "Empfangene Nachricht: %s\n", argv[i]);*/
     
   if(*argv[0] == '+')
     return size;
@@ -153,10 +136,10 @@ receive(int socketFD, char *msg, char *argv[]){
 }
 
 int
-sendToServer(int socketFD, char *msg){
-  int size = strlen(msg);
-  fprintf(stdout, "Sende %s", msg);
-  if(send(socketFD, msg, size, 0) == -1){
+sendToServer(int socketFD, char *string){
+  int size = strlen(string);
+  fprintf(stdout, "Sende %s", string);
+  if(send(socketFD, string, size, 0) == -1){
     perror("send() gescheitert");
     return -1;
   }
@@ -164,35 +147,35 @@ sendToServer(int socketFD, char *msg){
 }
 
 int
-checkMsg(char *msg){
-  if(strcmp(msg, "ENDPLAYERS") == 0)
+checkMsg(char *string){
+  if(strcmp(string, "ENDPLAYERS") == 0)
     return 0;
-  else if(strcmp(msg, "PLAYING") == 0)
+  else if(strcmp(string, "PLAYING") == 0)
     return 1;
-  else if(strcmp(msg, "YOU") == 0)
+  else if(strcmp(string, "YOU") == 0)
     return 2;
-  else if(strcmp(msg, "TOTAL") == 0)
+  else if(strcmp(string, "TOTAL") == 0)
     return 3; 
   else return -1;
 }
 
 int
-printPlayers(int count, char *argv[]){
+printPlayers(int count, char *vector[]){
   int nr = count - 1;
   char *farbe, *bereitschaft;
   
-  farbe = (strcmp(argv[2], "white")) ? "Schwarz" : "Weiss"; // errorhandling
-  bereitschaft = (atoi(argv[3])) ? "ist bereit" : "ist nicht bereit";
+  farbe = (strcmp(vector[2], "white")) ? "Schwarz" : "Weiss"; // errorhandling
+  bereitschaft = (atoi(vector[3])) ? "ist bereit" : "ist nicht bereit";
   
-  fprintf(stdout, "Spieler Nr. %i spielt %s und %s.\n", atoi(argv[1]), farbe, bereitschaft);
+  fprintf(stdout, "Spieler Nr. %i spielt %s und %s.\n", atoi(vector[1]), farbe, bereitschaft);
   return nr;
 }
 
 int
-stringSplit(char *string, char *argv[], char *delim){ 
+stringSplit(char *string, char *vector[], char *delim){ 
   int i = 0;
-  argv[i] = strtok(string, delim);
-  while(argv[i] != NULL)
-    argv[++i] = strtok(NULL, delim);
+  vector[i] = strtok(string, delim);
+  while(vector[i] != NULL)
+    vector[++i] = strtok(NULL, delim);
   return i;
 }
--- performConnection.h
+++ performConnection.h
@@ -3,15 +3,14 @@
 
 #define GAMEKINDNAME "Dame"
 #define BUFLEN 64
-#define ARGS 8
 #define VERSION "1.0"
 
 int performConnection(int socketFD, char *gameID);
 int openingHandler(int socketFD, char *gameID);
-int receive(int socketFD, char *msg, char *argv[]);            // empfaengt und splittet
-int sendToServer(int socketFD, char *msg);                    // sendet und fehlert bei Fehlschlag
-int checkMsg(char *msg);                                      // checkt Servernachricht
-int printPlayers(int count, char *argv[]);                     // printet Spielerinfo
-int stringSplit(char *string, char *argv[], char *delim);      // returnt argc
+int receive(int socketFD);            // empfaengt und splittet
+int sendToServer(int socketFD, char *string);                    // sendet und fehlert bei Fehlschlag
+int checkMsg(char *string);                                      // checkt Servernachricht
+int printPlayers(int count, char *vector[]);                     // printet Spielerinfo
+int stringSplit(char *string, char *vector[], char *delim);      // returnt argc
 
 #endif
